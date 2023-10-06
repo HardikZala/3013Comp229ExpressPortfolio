4ac2f64e87fabc876bfc9a9956b1ad83d47c5212
Hardik Zala <hz6314@gmail.com>
2023-10-05 23:02:19 -0400

    final commit 11:02

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 // Import necessary modules
 var express = require('./config/express');
+
+// Create an Express.js application instance
 var app = express();
 
-// Configure Express to serve static files from a "public" directory
-app.use('/Content', express.static('public/Content'));
+// Export the Express app for use in other modules
+module.exports = app;
 
+// Start the server on port 5000
+app.listen(5000, function () {
   console.log('Server running at http://localhost:5000/');
+});
